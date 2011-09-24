4b1cc5971919fe7a8bed18629ef1798ae428af3b
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-09-24 16:06:09 -0400

    regex searches f t w!

---
+++ examples/regex/with-regex.js
@@ -0,0 +1,16 @@
+
+/*
+Tweets from without the regex
+*/
+
+var search = require("twitter-search");
+
+search( { q : 'from:kisshotch+#nodejs', regex : /#UnitTests/gi }, function(error, tweets, tweetCount) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log("Tweets: ", tweetCount);
+	}
+});
+
+/* EOF */
