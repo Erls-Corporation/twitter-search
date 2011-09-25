429484b0cf386b7ba1711201e1578d4a7247dfb6
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-09-24 21:07:26 -0400

    benchmarking ++ memory usage

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -11,19 +11,19 @@ var i = total;
 console.log("[twitter search] ".green + "starting benchmarking");
 
 while (i--) {
-	search( { q : 'twitter', regex : /#API/gi }, function(error, tweets, tweetCount) {
+	search( { q : 'twitter', regex : /[the]/gi }, function(error, tweets, tweetCount) {
 		if (error) {
 			console.error("[twitter search] ".green + error.message);
+		} else {
+			if (i === 0) {
+				var time = (new Date - start) / 1000;
+				var MB = memory();
+				console.log("[twitter search] ".green +  "Memory usage: " + MB);
+				console.error("[twitter search] ".green + "Searches with Regular Expressions took %d seconds for %d searches (%d SPS)", time, total, total/time);
+				process.exit(0);
+			};
 		}
 	});
 };
-
-var time = (new Date - start) / 1000;
-
-var MB = memory();
-console.log("[twitter search] ".green +  "Memory usage: " + MB);
-console.error("[twitter search] ".green + "Searches with Regular Expressions took %d seconds for %d searches (%d SPS)", time, total, total/time);
-
-process.exit(0);
 
 /* EOF */
