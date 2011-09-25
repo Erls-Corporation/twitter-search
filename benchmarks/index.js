48395109bae561a80edbf804ad4ea9113e168496
Edward Hotchkiss <e@ingk.com>
2011-09-24 21:01:59 -0400

    benchmark, memory - 1000 async twitter searches with regex is ~66.5 MB memory

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,6 +1,7 @@
 
 // thanks mongoose
 var search = require("../");
+var memory = require("../lib/memory");
 require("colors");
 
 var start = new Date;
@@ -11,14 +12,18 @@ console.log("[twitter search] ".green + "starting benchmarking");
 
 while (i--) {
 	search( { q : 'twitter', regex : /#API/gi }, function(error, tweets, tweetCount) {
-	if (error) {
-		console.error("[twitter search] ".green + error.message);
-	}
-	// do do do
-});
-}
+		if (error) {
+			console.error("[twitter search] ".green + error.message);
+		}
+	});
+};
+
 var time = (new Date - start) / 1000;
+
+var MB = memory();
+console.log("[twitter search] ".green +  "Memory usage: " + MB);
 console.error("[twitter search] ".green + "Searches with Regular Expressions took %d seconds for %d searches (%d SPS)", time, total, total/time);
+
 process.exit(0);
 
 /* EOF */
