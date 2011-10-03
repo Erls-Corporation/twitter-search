af07c9c02ac4ac32e8dfe8896cf5c44eef222a20
Edward Hotchkiss <e@ingk.com>
2011-10-03 16:42:54 -0400

    makefile for vows, test

--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,43 @@
 
+// testing
+var vows = require("vows");
+var assert = require("assert");
+var should = require("should");
+
+// twitter-search core
 var search = require("../");
 
-var work = [
-	"module",
-	"node",
-	"js",
-	"klout",
-	"github",
-	"mongo",
-	"api",
-	"npm",
-	"startup",
-	"twitter",
-	"facebook",
-];
-
-var config = {
-	filter : work,
-	klout : true,
-	sorting : function(a, b) {
-		var x = a["klout"];
-    	var y = b["klout"];
-    	return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-	},
-	singlePage : true
+// wrapped search for testing
+function search() {
+  var work = ["module","node"];
+  var config = {
+    filter : work,
+    klout : true,
+    sorting : function(a, b) {
+      var x = a["klout"];
+        var y = b["klout"];
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+    },
+    singlePage : true
+  };
+  search( { q : "from:kisshotch", regex : /^/ }, config, function(error, tweets, tweetCount) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("tweets:",tweetCount);
+    };
+  });
 };
 
-search( { q : "from:kisshotch", regex : /^/ }, config, function(error, tweets, tweetCount) {
-	if (error) {
-		console.error(error);
-	} else {
-		console.log("tweets:",tweetCount);
-	};
-});
+vows.describe("Basic Twitter-Search module tests").addBatch({
+  'when dividing a number by zero': {
+    topic: function () { 
+      return 42 / 0 
+    },
+    'we get Infinity': function (topic) {
+      assert.equal (topic, Infinity);
+    }
+  }
+}).export(module);
 
 /* EOF */
