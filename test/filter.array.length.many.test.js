8a570adc034c683ba700b26265b64b55ff65aee0
Edward Hotchkiss <e@ingk.com>
2011-10-05 19:43:37 -0400

    some really solid tests here. check it

--- test/filter.array.length.many.test.js
+++ test/filter.array.length.many.test.js
@@ -7,12 +7,13 @@ var should = require("should");
 // twitter-search core
 var search = require("../");
 
-vows.describe("RegEx Tests").addBatch({
-  "When performing a search with a regex looking for node or mongo which I'm always talking about" : {
+vows.describe("Noise Filter").addBatch({
+  "When performing a search with a noise filter that is an 'Array' of length of more than one" : {
     topic : function() {
       var config = {
         query : "from:kisshotch",
-        regex : /node|mongo/gi
+        regex : /node|mongo/gi,
+        filter : ["cancer", "ingk"]
       };
       search(config, this.callback);
     },
