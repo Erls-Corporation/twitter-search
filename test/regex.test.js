449eaa07bcf0a0bf89b5680aa7788852dbc8ce23
Edward Hotchkiss <e@ingk.com>
2011-10-05 19:13:50 -0400

    new regex tests

--- test/regex.test.js
+++ test/regex.test.js
@@ -7,15 +7,16 @@ var should = require("should");
 // twitter-search core
 var search = require("../");
 
-vows.describe("Twitter Search [Basic]").addBatch({
-  "when performing a search" : {
+vows.describe("RegEx Tests").addBatch({
+  "When performing a search with a regex looking for node or mongo which I'm always talking about" : {
     topic : function() {
       var config = {
-        query : "from:kisshotch"
+        query : "from:kisshotch",
+        regex : /node|mongo/gi
       };
       search(config, this.callback);
     },
-    "we should get back no errors" : function(error, tweets, count) {
+    "We should get back no errors" : function(error, tweets, count) {
       assert.isNull(error);
     },
     "tweets should be an object" : function(error, tweets, count) {
@@ -23,6 +24,9 @@ vows.describe("Twitter Search [Basic]").addBatch({
     },
     "count should be a number" : function(error, tweets, count) {
       count.should.be.a("number");
+    },
+    "Result set should not be empty" : function(error, tweets, count) {
+      tweets.should.not.be.empty;
     }
   }
 }).export(module);
