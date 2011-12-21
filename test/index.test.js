0ae9be9c6dabc4113f4100c676ed051b11fd2c24
Edward Hotchkiss <e@ingk.com>
2011-12-21 16:16:20 -0500

    travis up to 0.8.x, formating

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,35 @@
 
 // testing
-var vows = require("vows");
-var assert = require("assert");
-var should = require("should");
+var vows = require('vows');
+var assert = require('assert');
+var should = require('should');
 
 // twitter-search core
-var search = require("../lib/twitter-search");
+var search = require('../lib/twitter-search');
 
-vows.describe("General Module Tests").addBatch({
-  "when instantiating twitter-search" : {
+vows.describe('General Module Tests').addBatch({
+  'when instantiating twitter-search' : {
     topic : function() { 
       return search;
     },
-    "search should be a function" : function (topic) {
-      topic.should.be.a("function");
+    'search should be a function' : function (topic) {
+      topic.should.be.a('function');
     },
   },
-  "when performing a search without a config set" : {
+  'when performing a search without a config set' : {
     topic : function() {
       search(null, this.callback);
     },
-    "should have error errors" : function(error, tweets, count) {
+    'should have error errors' : function(error, tweets, count) {
       assert.isNotNull(error);
     },
-    'should receive error message "config needs to be defined!"': function(error, tweets, count) {
-      assert.equal(error.message, "config needs to be defined!");
+    'should receive error message \'config needs to be defined!\'': function(error, tweets, count) {
+      assert.equal(error.message, 'config needs to be defined!');
     },
-    "tweets should be null" : function(error, tweets, count) {
+    'tweets should be null' : function(error, tweets, count) {
       assert.isNull(tweets);
     },
-    "count should be null" : function(error, tweets, count) {
+    'count should be null' : function(error, tweets, count) {
       assert.isNull(count);
     }
   }
