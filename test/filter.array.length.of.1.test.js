5494198b0984df48c0ac358262c6e33a8c848fe2
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-12-21 16:16:20 -0500

    travis up to 0.8.x, formating

--- test/filter.array.length.of.1.test.js
+++ test/filter.array.length.of.1.test.js
@@ -1,32 +1,32 @@
 
 // testing
-var vows = require("vows");
-var assert = require("assert");
-var should = require("should");
+var vows = require('vows');
+var assert = require('assert');
+var should = require('should');
 
 // twitter-search core
-var search = require("../lib/twitter-search");
+var search = require('../lib/twitter-search');
 
-vows.describe("Noise Filter").addBatch({
-  "When performing a search with a noise filter that is an 'Array' of length of 1 only" : {
+vows.describe('Noise Filter').addBatch({
+  'When performing a search with a noise filter that is an \'Array\' of length of 1 only' : {
     topic : function() {
       var config = {
-        query : "from:nodejs",
+        query : 'from:nodejs',
         regex : /node|mongo/gi,
-        filter : ["cancer"]
+        filter : ['cancer']
       };
       search(config, this.callback);
     },
-    "we should get back no errors" : function(error, tweets, count) {
+    'we should get back no errors' : function(error, tweets, count) {
       assert.isNull(error);
     },
-    "tweets should be an object" : function(error, tweets, count) {
-      tweets.should.be.a("object");
+    'tweets should be an object' : function(error, tweets, count) {
+      tweets.should.be.a('object');
     },
-    "count should be a number" : function(error, tweets, count) {
-      count.should.be.a("number");
+    'count should be a number' : function(error, tweets, count) {
+      count.should.be.a('number');
     },
-    "result set should not be empty" : function(error, tweets, count) {
+    'result set should not be empty' : function(error, tweets, count) {
       tweets.should.not.be.empty;
     }
   }
--- test/search.test.js
+++ test/search.test.js
@@ -1,28 +1,28 @@
 
 // testing
-var vows = require("vows");
-var assert = require("assert");
-var should = require("should");
+var vows = require('vows');
+var assert = require('assert');
+var should = require('should');
 
 // twitter-search core
-var search = require("../lib/twitter-search");
+var search = require('../lib/twitter-search');
 
-vows.describe("Twitter Search [Basic]").addBatch({
-  "when performing a search" : {
+vows.describe('Twitter Search [Basic]').addBatch({
+  'when performing a search' : {
     topic : function() {
       var config = {
-        query : "from:nodejs"
+        query : 'from:nodejs'
       };
       search(config, this.callback);
     },
-    "we should get back no errors" : function(error, tweets, count) {
+    'we should get back no errors' : function(error, tweets, count) {
       assert.isNull(error);
     },
-    "tweets should be an object" : function(error, tweets, count) {
-      tweets.should.be.a("object");
+    'tweets should be an object' : function(error, tweets, count) {
+      tweets.should.be.a('object');
     },
-    "count should be a number" : function(error, tweets, count) {
-      count.should.be.a("number");
+    'count should be a number' : function(error, tweets, count) {
+      count.should.be.a('number');
     }
   }
 }).export(module);
