5ed31e7bae35351458785714250b365f98570576
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-12-25 14:42:25 -0500

    remove search.test.js, merge tests to increase test speeds

--- test/index.test.js
+++ test/index.test.js
@@ -9,7 +9,7 @@ var search = require('../lib/twitter-search');
 
 vows.describe('general module tests').addBatch({
   'when instantiating twitter-search' : {
-    topic : function() { 
+    topic:function() { 
       return search;
     },
     'search should be a function' : function (topic) {
@@ -17,7 +17,7 @@ vows.describe('general module tests').addBatch({
     },
   },
   'when performing a search without a config set' : {
-    topic : function() {
+    topic:function() {
       search(null, this.callback);
     },
     'should have error errors' : function(error, tweets, count) {
@@ -31,6 +31,24 @@ vows.describe('general module tests').addBatch({
     },
     'count should be null' : function(error, tweets, count) {
       assert.isNull(count);
+    }
+  }
+}).describe('twitter search [basic]').addBatch({
+  'when performing a search':{
+    topic:function(){
+      var config = {
+        query:'from:nodejs'
+      };
+      search(config, this.callback);
+    },
+    'we should get back no errors':function(error, tweets, count){
+      assert.isNull(error);
+    },
+    'tweets should be an object':function(error, tweets, count){
+      tweets.should.be.a('object');
+    },
+    'count should be a number':function(error, tweets, count){
+      count.should.be.a('number');
     }
   }
 }).export(module);
